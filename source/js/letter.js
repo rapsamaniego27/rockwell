94a10db3b46f7f416acf57b522dafaf3082de086
Ralph Samaniego <rapsamaniego27@gmail.com>
2022-04-27 20:46:03 +0800

    changed alt and images

--- source/js/letter.js
+++ source/js/letter.js
@@ -9,27 +9,28 @@ class Letter {
   showHug() {
     const hugButton = document.querySelector('#btnHug');
 
-    hugButton.addEventListener('click', (e)=> {
+    /* hugButton.addEventListener('click', (e)=> {
       e.preventDefault();
       this.modalHugOverlay.classList.remove('modal--hide');
 
 
-      document.querySelector(".hug").addEventListener("submit", handleSubmit);
+    }); */
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        let myForm = document.getElementById(".hug");
-        let formData = new FormData(myForm);
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(formData).toString(),
-        })
-          .then(() => console.log("Form successfully submitted"))
-          .catch((error) => alert(error));
-      };
-      
-    });
+    document.querySelector(".hug").addEventListener("submit", handleSubmit);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      this.modalHugOverlay.classList.remove('modal--hide');
+      let myForm = document.getElementById(".hug");
+      let formData = new FormData(myForm);
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      })
+        .then(() => console.log("Form successfully submitted"))
+        .catch((error) => alert(error));
+    };
 
   }
 
